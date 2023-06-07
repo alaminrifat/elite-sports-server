c205e9e381a4c4c8f25a08f8d39c8a36a68563b6
alaminrifat <alaminrifat71@gmail.com>
2023-06-08 03:02:08 +0600

    user post

--- index.js
+++ index.js
@@ -41,13 +41,26 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         //  client.connect();
         const usersCollection = client.db("eliteSportsDB").collection("users");
-        const classesCollection = client.db("eliteSportsDB").collection("classes");
-        const instructorsCollection = client.db("eliteSportsDB").collection("instructors");
+        const classesCollection = client
+            .db("eliteSportsDB")
+            .collection("classes");
+        const instructorsCollection = client
+            .db("eliteSportsDB")
+            .collection("instructors");
 
-       
+        // store an user to the database
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            console.log(user);
+            if (existingUser) {
+                return res.send({ message: "User already exists!" });
+            }
 
-
- 
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -60,7 +73,6 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
     res.send("Elite Sports Academy Server is Running...");
